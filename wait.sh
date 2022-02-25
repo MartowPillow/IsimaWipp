a80c044939ba832b066c7c74016685ca5c153b8d
Thomas Yriarte <56124625+MartowPillow@users.noreply.github.com>
2022-02-26 00:17:06 +0100

    only wait on wipp pods

--- wait.sh
+++ wait.sh
@@ -1,7 +1,7 @@
 wait=1
 echo waiting for pods readyness 
 while [[ $wait -gt 0 ]]; do
-    wait=$(kubectl get pods -o json | yq '.items[] | select(.status.containerStatuses[0].ready==false)' | wc -l)
+    wait=$(kubectl get pods -l source=wipp -o json | yq '.items[] | select(.status.containerStatuses[0].ready==false)' | wc -l)
     sleep 2
     echo -n .
 done
